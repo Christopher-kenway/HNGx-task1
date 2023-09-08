51faccf1071ce9b2c6e00d31ab9f84658b7a2f79
Christopher-kenway <102259049+Christopher-kenway@users.noreply.github.com>
2023-09-08 13:28:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-const day = new Date();
-let currentDay = days[day.getDay()];
-document.querySelector('[data-testid="currentDayOfTheWeek"]').innerHTML = `[ current day: ${currentDay} ]`;
-
-
-
-const updateCurrentUTCTime = () => {
-    const newCurrentUTCTime = new Date().getTime();
-  
-    document.querySelector("[data-testid='currentUTCTime']").textContent = `[ current time: ${newCurrentUTCTime} ]`;
-  };
-  
-  setInterval(updateCurrentUTCTime, 1000);
+        const day = new Date();
+        let currentDay = days[day.getDay()];
+        document.querySelector('[data-testid="currentDayOfTheWeek"]').innerHTML = `[ Current day: ${currentDay} ]`;
+       
+        const updateCurrentUTCTime = () => {
+            const currentTime = new Date();
+            const hours = currentTime.getUTCHours().toString().padStart(2, '0');
+            const minutes = currentTime.getUTCMinutes().toString().padStart(2, '0');
+            const seconds = currentTime.getUTCSeconds().toString().padStart(2, '0');
+            const milliseconds = currentTime.getUTCMilliseconds().toString().padStart(3, '0');
+            const formattedTime = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+        
+            document.querySelector("[data-testid='currentUTCTime']").textContent = `[ Current time (UTC): ${formattedTime} ]`;
+        };
+        
+        setInterval(updateCurrentUTCTime, 100);
+        
+        
