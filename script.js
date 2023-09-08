a46e29a4840ba502ff7ea0a3ed51c660ca25e705
Christopher-kenway <102259049+Christopher-kenway@users.noreply.github.com>
2023-09-08 11:47:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", 
 
 const day = new Date();
 let currentDay = days[day.getDay()];
-document.querySelector('[data-testid="currentDayOfTheWeek"]').innerHTML = `[ ${currentDay} ]`;
+document.querySelector('[data-testid="currentDayOfTheWeek"]').innerHTML = `[ current day: ${currentDay} ]`;
 
 
 
-const utcTime = new Date().getTime();
-
-document.querySelector('[data-testid="currentUTCTime"]').textContent = `[ ${utcTime} ]`;
-        
-
-
-
+const updateCurrentUTCTime = () => {
+    const newCurrentUTCTime = new Date().getTime();
+  
+    document.querySelector("[data-testid='currentUTCTime']").textContent = `[ current time: ${newCurrentUTCTime} ]`;
+  };
+  
+  setInterval(updateCurrentUTCTime, 1000);
